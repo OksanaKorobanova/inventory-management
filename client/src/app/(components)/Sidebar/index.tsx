5befa93540be5d0cda7665c52c9ca5f8725747b7
Oksana Korobanova (Vitol) <58445023+OksanaKorobanova@users.noreply.github.com>
2024-09-12 18:40:58 +0200

    updated images

--- client/src/app/(components)/Sidebar/index.tsx
+++ client/src/app/(components)/Sidebar/index.tsx
@@ -12,6 +12,7 @@ import {
   SlidersHorizontal,
   User,
 } from 'lucide-react';
+import Image from 'next/image';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 import React from 'react';
@@ -75,7 +76,13 @@ const Sidebar = () => {
         className={`flex gap-3 justify-between md:justify-normal items-center pt-8 ${
           isSidebarCollapsed ? 'px-5' : 'px-8'
         }`}>
-        <div>logo</div>
+        <Image
+          src='https://s3-koro-inventorymanagement.s3.amazonaws.com/logo.png'
+          alt='Edstock logo'
+          height={27}
+          width={27}
+          className='rounded w-8'
+        />
         <h1
           className={`font-extrabold text-2xl ${
             isSidebarCollapsed ? 'hidden' : 'block'
